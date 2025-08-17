d63576ed16aa43940293c2429fe5568143de9753
Bipin-Kumar-Yadav <krbipin25055@gmail.com>
2025-08-17 18:32:00 +0530

    added oauth2 configuration

---
+++ src/main/java/com/expence/gateway_authserver/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.expence.gateway_authserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+            security
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                    req
+                        .requestMatchers("/api/v1/users/*")
+                        .authenticated()
+                        .anyRequest().permitAll()
+                );
+            
+            security
+                .oauth2ResourceServer(authServer ->
+                    authServer.jwt(Customizer.withDefaults())
+                );
+
+            return security.build();
+    }
+}
