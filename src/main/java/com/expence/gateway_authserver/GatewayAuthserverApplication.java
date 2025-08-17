10f50d97381bc89909ce1d0b37165dc2acd26f4b
Bipin-Kumar-Yadav <krbipin25055@gmail.com>
2025-08-17 18:08:41 +0530

    initail commit

---
+++ src/main/java/com/expence/gateway_authserver/GatewayAuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.expence.gateway_authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayAuthserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expence/gateway_authserver/GatewayAuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expence.gateway_authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
