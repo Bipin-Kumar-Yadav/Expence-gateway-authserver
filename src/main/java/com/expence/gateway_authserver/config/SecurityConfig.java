1c09aceb30add8dd69662e839e53bb7f5812f530
Bipin-Kumar-Yadav <krbipin25055@gmail.com>
2025-08-17 19:08:23 +0530

    convrted it in eureka client and api gateway with oauth2 server

--- src/main/java/com/expence/gateway_authserver/config/SecurityConfig.java
+++ src/main/java/com/expence/gateway_authserver/config/SecurityConfig.java
@@ -3,24 +3,23 @@ package com.expence.gateway_authserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity security) throws Exception {
             security
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req ->
+                .cors(cors-> cors.disable())
+                .csrf(csrf-> csrf.disable())
+                .authorizeExchange(req ->
                     req
-                        .requestMatchers("/api/v1/users/*")
+                        .pathMatchers("/api/v1/users/*")
                         .authenticated()
-                        .anyRequest().permitAll()
+                        .anyExchange().permitAll()
                 );
             
             security
